67b1446e42f0bef2fc7b9e109252700d7ddd6118
Sahiti-Kambham <78361096+Sahiti-Kambham@users.noreply.github.com>
2021-09-27 21:48:08 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+function setup() {
+  canvas = createCanvas(300, 300);
+  canvas.center();
+  video = createCapture(VIDEO);
+  video.hide();
+  classifier=ml5.imageClassifier('MobileNet', modeloaded);
+}
+
+function modeloaded() {
+  console.log("modeloaded");
+}
+
+function draw() {
+  image(video, 0, 0, 300, 300);
+  classifier.classify(video, gotresult)
+}
+
+function gotresult(error, results) {
+  if (error) {
+   console.error(error);
+  }
+  else {
+    console.log(results);
+    document.getElementById("EJECT").innerHTML=results[0].label;
+    document.getElementById("ACCRACE").innerHTML=results[0].confidence.toFixed(3);
+  }
+}
